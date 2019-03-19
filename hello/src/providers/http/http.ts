a5e7ef1aead039952b5704a59ad61c59991d51d3
hauck <thiagoahauck@gmal.com>
2019-03-19 09:23:11 -0300

    Aula 18/03/2019

--- hello/src/providers/http/http.ts
+++ hello/src/providers/http/http.ts
@@ -9,22 +9,72 @@ export class HttpProvider {
     public url : string;
     public token : string;
 
-    //private const TIMEOUT : number = 15000;
+    private TIMEOUT = 15000;
 
-    constructor(){
+    constructor(private http: Http){
         this.url = null;
         this.token = null;
     }
+
+    private prepareHeaders(contentType : boolean){
+        let headers = new Headers();
+        headers.append('Accept', 'application/json');
+    
+        if (contentType){
+          headers.append('Content-Type', 'application/json');
+        }
+    
+        if (this.token == ''){
+          this.token = null;
+        }
+    
+        if (this.token != null){
+          headers.append('Authorization', 'bearer ' + this.token);
+        }
+    
+        return headers;
+      }
     public get(){
-
+        let headers = this.prepareHeaders(false);
+        return this.http.get(this.url, {headers: headers}).timeout(this.TIMEOUT).map((res : Response) => res.json());
     }
-    public post(obj : any){
+    public post(object : any){
+        let body : string = '';
 
+        let headers = this.prepareHeaders(true);
+    
+        if (object != null && object != undefined){
+          body = JSON.stringify(object);
+        }
+    
+        return this.http.post(
+                            this.url, body, {headers: headers}
+                            ).timeout(
+                                this.TIMEOUT
+                                ).map(
+                                    (res : Response) => res.json()
+                                    );
     }
-    public put(obj : any){
+    public put(object : any){
+        let body : string = '';
 
+        let headers = this.prepareHeaders(true);
+    
+        if (object != null && object != undefined){
+          body = JSON.stringify(object);
+        }
+    
+        return this.http.put(this.url, body, {headers: headers}).timeout(this.TIMEOUT);;
     }
-    public delete(obj : any){
+    public patch(object : any){
+        let body : string = '';
 
+        let headers = this.prepareHeaders(true);
+    
+        if (object != null && object != undefined){
+          body = JSON.stringify(object);
+        }
+    
+        return this.http.patch(this.url, body, {headers: headers}).timeout(this.TIMEOUT);;
     }
 }
